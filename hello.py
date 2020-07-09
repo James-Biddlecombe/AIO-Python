f2810cbbf27c8e8111251fa9f03f35369e6df911
jamesb <irbiddlecombe@gmail.com>
2020-07-09 12:23:25 +0200

    Update hello.py
    saving work

--- hello.py
+++ hello.py
@@ -38,4 +38,89 @@ print(f"Hello {username}")
 unit_price = 49.99
 quantity = 30
 print(f"Subtotal: ${unit_price * quantity}")
+print(f"Subtotal: ${unit_price * quantity:,.2f}") #2 decimal places fixed
+
+#formatting percent numbers
+sales_tax_rate = 0.065
+print(f"Sales Tax Rate {sales_tax_rate}")
+print(f"Sales Tax Rate {sales_tax_rate:.2%}")
+sample1 = f'Sales Tax Rate {sales_tax_rate:.2%}'
+sample2 = f"Sales Tax Rate {sales_tax_rate:.2%}"
+sample3 = f"""Sales Tax Rate {sales_tax_rate:.2%}"""
+sample4 = f'''Sales Tax Rate {sales_tax_rate:.2%}'''
+print(sample1)
+print(sample2)
+print(sample3)
+print(sample4)
+
+#multiline f string example
+unit_price = 49.95
+quantity = 32
+sales_tax_rate= 0.065
+subtotal = quantity * unit_price
+sales_tax = sales_tax_rate * subtotal
+total = subtotal + sales_tax
+output = f"""
+Subtotal:   ${subtotal:,.2f}
+Sales Tax:  ${sales_tax:,.2f}
+Total:      ${total:,.2f}
+"""
+print(output)
+
+#formatting width and alignment
+output = f"""
+Subtotal:   ${subtotal:>9,.2f}
+Sales Tax:  ${sales_tax:>9,.2f}
+Total:      ${total:>9,.2f}
+"""
+print(output)
+
+#hex numbers
+print("hex of 255 is: ", hex(255))
+
+#complex numbers
+z = complex(2,-3)
+print("complex(2,-3) is ", z)
+print(z.real)
+print(z.imag)
+
+#manipulating strings
+first_name = "Alan"
+middle_init = "C"
+last_name = "Simpson"
+full_name = first_name + " " + middle_init + ". " + last_name
+print(full_name)
+
+#getting the length of a string
+s1 = ""
+s2 = " "
+s3 = "A B C"
+print(len(s1))
+print(len(s2))
+print(len(s3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
